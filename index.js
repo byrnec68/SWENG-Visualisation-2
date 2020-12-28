3637bde3a3541fe4314c69ca8697fdb8a25d6b83
byrnec68 <byrnec68@tcd.ie>
2020-12-28 21:06:40 +0000

    Chugging along

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 const btnSubmit = document.getElementById("btnSubmit")
-btnSubmit.addEventListener("click",alertTheLads)
-function alertTheLads(){
-    let x = prompt("Is this working?")
+btnSubmit.addEventListener("click",inputHandler)
+
+
+
+function inputHandler(){
+    let username = prompt("Enter Username")
+    //mainFun(username)
+}
+
+/*
+Main 
+*/ 
+async function main(username){
+    //url
+    let url = `https://api.github.com/users/${username}/repos`
+    let repoData = await getRequest(url).catch(error => console.error(error))
+
+    //TreeMap
+    languageTreeMap(repoData, username)
+
+    //Social Graph
+    parseSocialData(repoData)
+}
+
+/*
+Get Request
+*/
+async function getRequest(url){
+    const response = await fetch(url)
+    let result = await response.json()
+    return result
 }
